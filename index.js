a6ec3aaa8763e8ee3f2e4758308f914327e1ed3f
Kayac <egg734631@gmail.com>
2019-08-19 17:01:57 +0800

    Add

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const runBGTasks = require('./src/task');
 const findProducts = require('./src/database/sqlite/findProducts');
 const {log} = require('./src/util/str');
 
+const cluster = require('cluster');
+const os = require('os');
+
 require('dotenv').config();
 
 // ===================================
@@ -20,21 +23,36 @@ const {
 async function main() {
     const databases = await DataBases();
 
-    const server = Server({databases});
+    if (cluster.isMaster) {
+        const cpuCount = os.cpus().length;
 
-    server.listen(SERVER_PORT, onStart);
+        for (let i = 0; i < cpuCount; i++) {
+            cluster.fork()
+        }
 
-    const products =
-        findProducts(databases.domain)
-            .map(({name}) => name);
-
-    function onStart() {
-        log(`${SERVER_NAME} listening at ${server.url}`);
+        const products =
+            findProducts(databases.domain)
+                .map(({name}) => name);
 
         runBGTasks(products);
+
+        cluster.on('exit', (worker) => {
+            log(`Worker: ${worker.id} is Exit !!!`);
+
+            cluster.fork()
+        });
+
+    } else {
+        const server = Server({databases});
+
+        server.listen(SERVER_PORT, onStart);
+
+        function onStart() {
+            log(`${SERVER_NAME} listening at ${server.url}`);
+        }
     }
+
 }
 
 // ===================================
-
-main();
+main()
